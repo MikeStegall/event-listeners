1037073c06751ca759f4c1c0a54c78a5aec2f6aa
MikeStegall <mestegall5@gmail.com>
2017-04-02 18:23:39 -0500

    Finished task 2 and 3

--- main.js
+++ main.js
@@ -13,3 +13,40 @@ document.querySelector('#hide-nav button').addEventListener('click', function ()
     navMenu.style.visibility = 'visible'
   }
 })
+
+// Task 2
+// add items to guest list
+document.querySelector('#add-guest input').addEventListener('keypress', function (x) {
+  var liEl = document.createElement('li')
+  var ulEl = document.querySelector('#add-guest .guest-list')
+  liEl.classList.add('guest')
+  if (x.keyCode === 13) {
+    if (this.value !== '') {
+      liEl.textContent = this.value
+      ulEl.appendChild(liEl)
+      this.value = ''
+    }
+  }
+})
+
+// Task 3
+// add a remove button to every guest that is inputted.
+document.querySelector('#add-guest-bonus input').addEventListener('keypress', function (x) {
+  var liEl = document.createElement('li')
+  var ulEl = document.querySelector('#add-guest-bonus .guest-list')
+  liEl.classList.add('guest')
+  if (x.keyCode === 13) {
+    if (this.value !== '') {
+      // create button element
+      var btn = document.createElement('button')
+      btn.textContent = 'X'
+      btn.addEventListener('click', function () {
+        this.parentElement.remove()
+      })
+      liEl.textContent = this.value
+      liEl.appendChild(btn)
+      ulEl.appendChild(liEl)
+      this.value = ''
+    }
+  }
+})
