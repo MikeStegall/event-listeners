c325c33b3ab3ce8a8aceceab8e1c9ac51cf83f24
MikeStegall <mestegall5@gmail.com>
2017-04-02 16:40:21 -0500

    Finished the first task and added .editorconfig and READMD.md

---
+++ README.md
@@ -0,0 +1,2 @@
+## Event Listeners
+This homework help out with learning more even listeners and changing the CSS or adding items to the DOM with them.
--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 // GO!
 
+// Task #1
+// Hide the nav menu
+document.querySelector('#hide-nav button').addEventListener('click', function () {
+  var navMenu = document.querySelector('.nav-menu')
+  var NavMenuVis = window.getComputedStyle(navMenu, null).getPropertyValue('visibility')
+  if (NavMenuVis === 'visible' && this.textContent === 'Hide nav bar') {
+    this.textContent = 'Show nav bar'
+    navMenu.style.visibility = 'hidden'
+  } else {
+    this.textContent = 'Hide nav bar'
+    navMenu.style.visibility = 'visible'
+  }
+})
